21408fb286fd415ee76b15d222a5878414fec934
Ramya Prasanna <ramya.prasanna06@gmail.com>
2025-06-25 11:08:58 +0530

    2nd draft
    fixed:
    
    1) clicking and changing the circle/x
    
    2) added a feature for draws
    
    3) Added headings to readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# learning-frontend
+# Two Player Tic Tac Toe Game
+
+## Overview
+
+## How It Was Built
+
+## Challenges Along the Way
+
+## Accomplishments I'm Proud Of
+
+## What I Learned
--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,7 @@ const win_msg = document.querySelectorAll('.win-msg');
 console.log(win_msg.length);
 
 let counter = 1;
+let gameOver = true;
 
 const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
@@ -16,58 +17,74 @@ player1.style.background = 'blue';
 player1.style.color = 'white';
 
 for (let i = 0; i < 9; i++) {
-        box[i].addEventListener('click', (e) => boxClicked(e, i));
+    box[i].addEventListener('click', (e) => boxClicked(e, i));
 }
 
 function boxClicked(e, i) {
-    if (counter % 2 === 1) {
-        console.log('Player 1 Turn');
-        box[i].style.backgroundImage = "url('blue-circle.png')";
-        box[i].style.backgroundSize = "contain";
-        box[i].style.backgroundRepeat = "no-repeat";
-        box[i].style.backgroundPosition = "center";
+    if (board[i] === 0) {
+        if (counter % 2 === 1) {
+            console.log('Player 1 Turn');
+            box[i].style.backgroundImage = "url('blue-circle.png')";
+            box[i].style.backgroundSize = "contain";
+            box[i].style.backgroundRepeat = "no-repeat";
+            box[i].style.backgroundPosition = "center";
 
-        player2.style.background = 'red';
-        player2.style.color = 'white';
-        player1.style.background = 'black';
-        player1.style.color = 'blanchedalmond';
-        
-        board[i] = 1;
-    } else {
-        console.log('Player 2 Turn');
-        box[i].style.backgroundImage = "url('red-x.png')";
-        box[i].style.backgroundSize = "contain";
-        box[i].style.backgroundRepeat = "no-repeat";
-        box[i].style.backgroundPosition = "center";
-        
-        player1.style.background = 'blue';
-        player1.style.color = 'white';
-        player2.style.background = 'black';
-        player2.style.color = 'blanchedalmond';
-
-        board[i] = 2;
-    }
-    
-    const winner = playerWin();
-    if (winner !== 0) {
-        if (winner === 1) {
-            console.log('Congrats Player 1!');
-            win_msg[0].style.background = 'purple';
-            win_msg[0].style.color = 'white';
+            player2.style.background = 'red';
+            player2.style.color = 'white';
+            player1.style.background = 'black';
+            player1.style.color = 'blanchedalmond';
+            
+            board[i] = 1;
         } else {
-            console.log('Congrats Player 2!');
+            console.log('Player 2 Turn');
+            box[i].style.backgroundImage = "url('red-x.png')";
+            box[i].style.backgroundSize = "contain";
+            box[i].style.backgroundRepeat = "no-repeat";
+            box[i].style.backgroundPosition = "center";
+            
+            player1.style.background = 'blue';
+            player1.style.color = 'white';
+            player2.style.background = 'black';
+            player2.style.color = 'blanchedalmond';
+
+            board[i] = 2;
+        }
+        const winner = playerWin();
+        if (winner !== 0) {
+            if (winner === 1) {
+                console.log('Congrats Player 1!');
+                win_msg[0].style.background = 'purple';
+                win_msg[0].style.color = 'white';
+            } else {
+                console.log('Congrats Player 2!');
+                win_msg[2].style.background = 'purple';
+                win_msg[2].style.color = 'white';
+            }
+            reset_btn.style.background = 'palevioletred';
+            reset_btn.style.color = 'maroon';
+            reset_btn.addEventListener('click', reset);
+        }
+
+        for (let i = 0; i < 9; i++) {
+            if (board[i] === 0) {
+                gameOver = false;
+            }
+        }
+        if (gameOver) {
             win_msg[1].style.background = 'purple';
             win_msg[1].style.color = 'white';
+            reset_btn.style.background = 'palevioletred';
+            reset_btn.style.color = 'maroon';
+            reset_btn.addEventListener('click', reset);
+        } else {
+            gameOver = true;
         }
-        reset_btn.style.background = 'palevioletred';
-        reset_btn.style.color = 'maroon';
-        reset_btn.addEventListener('click', reset);
-    }
-    counter++;
 
-    console.log(counter);
-    console.log(board);
-    return counter;
+        counter++;
+        console.log(counter);
+        console.log(board);
+        return counter;
+    }
 }
 
 function playerWin() {
@@ -90,7 +107,7 @@ function playerWin() {
         (board[2] === 2 && board[5] === 2 && board[8] === 2) ||
         (board[0] === 2 && board[4] === 2 && board[8] === 2) ||
         (board[2] === 2 && board[4] === 2 && board[6] === 2)) {
-            return 1;
+            return 2;
     }
     return 0;
 }
